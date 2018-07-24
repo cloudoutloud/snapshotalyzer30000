b7eae7e3d0694c02406594025b4cbd8280a2c0c6
Sean Rigby <seanrigby@Diegos-MBP.localdomain>
2018-07-24 17:11:11 +0100

    rechanged shotty.py

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,17 +4,18 @@ import click
 session = boto3.Session(profile_name='snappysean')
 ec2 = session.resource('ec2')
 
+
 @click.command()
 def list_instances():
-    "List EC2 instances"
+    "list EC2 Instances yall"
     for i in ec2.instances.all():
         print(', '.join((
-        i.id,
-        i.instance_type,
-        i.placement['AvailabilityZone'],
-        i.state['Name'],
-        i.public_dns_name)))
-
+            i.id,
+            i.instance_type,
+            i.private_ip_address,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
     return
 
 if __name__ == '__main__':
